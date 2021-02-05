097edd854f38659bdf236d535ad5cd24e82a4d0d
Suresh Raju Pilli <sureshraju.pilli@gmail.com>
2021-02-05 11:17:08 +0530

    JumpingClouds definition

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace CSharp
+namespace PSRHackerRank
 {
     class Program
     {
         private static void Main(string[] args)
         {
             // Counting Valleys
-            Console.WriteLine("Valley Count: " + new CountOfValleys().countingValleys());
+            // Console.WriteLine("Valley Count: " + new CountOfValleys().countingValleys());
+
+            // Jumping Clouds
+            Console.WriteLine("Jumping on Clouds: " + new JumpingClouds().jumpingOnClouds());
         }
     }
 }
